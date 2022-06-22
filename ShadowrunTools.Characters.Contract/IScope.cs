0fa4e10e5657d75eb982acab667a6ac740ad9fa0
Bryan Jones <daedeross@gmail.com>
2022-06-22 15:34:42 -0500

    Added own DSL to replace ExpressionEvaluator

---
+++ ShadowrunTools.Characters.Contract/IScope.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShadowrunTools.Characters
+{
+    public interface IScope<out T>
+    {
+        T Me { get; }
+
+        ICategorizedTraitContainer Traits { get; }
+    }
+}
