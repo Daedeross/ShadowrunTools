3ed5cb1bedaf79f30b3c5e149a2827b52840e0bb
Bryan Jones <daedeross@gmail.com>
2022-07-27 16:34:02 -0500

    One more compile warning fixed, rest TODO

---
+++ ShadowrunTools.Foundation/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ShadowrunTools.Foundation
+{
+    public static class EnumerableExtensions
+    {
+        public static Dictionary<K, V> Copy<K, V>(this IEnumerable<KeyValuePair<K, V>> source)
+        {
+            return source.ToDictionary(
+                kvp => kvp.Key,
+                kvp => kvp.Value);
+        }
+    }
+}
